f0776a287bb5a1f1a77f26feaee082c62ae83ac5
aruna-sen <arunamcawins@gmail.com>
2021-04-27 18:06:05 +0200

    initial commit

---
+++ src/test/java/com/aruna/apitest/fcheck/test/TCGet.java
@@ -0,0 +1,36 @@
+package com.aruna.apitest.fcheck.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+public class TCGet {
+	@Test
+	void getReqCheck()
+	{
+		RestAssured.baseURI="https://reqres.in/";
+		RequestSpecification req=RestAssured.given();
+		Response res=req.request(Method.GET,"/api/users/2");
+		String str=res.getBody().asString();
+		System.out.println("body:"+str);
+		String ct=res.getHeader("Content-Type");
+	System.out.println(ct);
+Assert.assertEquals(ct,"application/json; charset=utf-8");
+String ctser=res.getHeader("Server");
+Assert.assertEquals(ctser,"cloudflare");
+int scod=res.getStatusCode();
+System.out.println("scod:"+scod);
+Assert.assertEquals(scod,200);
+long ti=res.getTime();
+System.out.println("time"+ti);
+String s=res.jsonPath().get("data.first_name");
+System.out.println(s);
+Assert.assertEquals(s,"Janet");
+	}
+
+}
